10cb01351ed348ae2b0b4b2e6bab52372d36ab1b
dblossom <daniel.blossom@gmail.com>
2015-01-01 23:08:09 +0000

    initial commit

---
+++ VideoPoker/videopoker/videopoker/Deck.java
@@ -0,0 +1,70 @@
+package videopoker;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+
+public class Deck {
+	
+	/**
+	 * The original deck
+	 */
+	List<Card> unshuffled;
+	
+	/**
+	 * The deck after being shuffled
+	 */
+	Queue<Card> shuffled;
+	
+	/**
+	 * Build a default deck of 52 cards and shuffle that deck
+	 */
+	public Deck(){
+		unshuffled = new ArrayList<Card>();
+		shuffled = new ArrayDeque<>();
+		buildDeck();
+		shuffle();
+	}
+	
+	/**
+	 * Builds a standard deck of 52 cards
+	 */
+	void buildDeck(){
+		// for each suit
+		for(Suit suit: Suit.values()){
+		    // for each value of that suit
+			int value = 1;
+			for(int i = 0; i < 13; i++){
+			    unshuffled.add(new Card(suit, value++));
+			}
+		}
+	}
+	
+	/**
+	 * Shuffles a deck
+	 */
+	void shuffle(){
+		Random shuffle = new Random(new GregorianCalendar().getTimeInMillis());
+		while(unshuffled.size() != 0){
+			int next = shuffle.nextInt(unshuffled.size());
+			shuffled.add(unshuffled.remove(next));
+		}
+	}
+	
+	/**
+	 * Simple tests
+	 * @param args
+	 */
+	public static void main(String[] args){
+		Deck d = new Deck();
+		
+		for(Card card: d.shuffled){
+			System.out.println(card.valueToString() + card.suitToChar());
+		}
+		
+		System.out.println("");
+	}
+}
---
+++ VideoPoker/videopoker/videopoker/Hand.java
@@ -0,0 +1,66 @@
+package videopoker;
+
+public class Hand {
+	
+	// A deck of cards
+	Deck deck = new Deck();
+	
+	// An array to hold this hand
+	Card[] hand = new Card[5];
+	
+	/**
+	 * Default constructor
+	 */
+	public Hand(){}
+	
+	/**
+	 * Deals out the deck - fills the hand array
+	 */
+	public void deal(){
+		
+		for(int i = 0; i < hand.length; i++){
+			hand[i] = deck.shuffled.remove();
+		}
+	}
+	
+	/**
+	 * Replaces a card in the hand with the next card
+	 * @param card the card position to be changed
+	 */
+	public void newCard(int card){
+		
+		hand[card] = deck.shuffled.remove();
+		
+	}
+	
+	/**
+	 * Prints out the hand, for testing really
+	 */
+	public void printHand(){
+	    
+		for(Card card: hand){
+		    System.out.println(card.valueToString() + card.suitToChar());
+		}
+	}
+	
+	/**
+	 * Main for testing ... 
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+		Hand h = new Hand();
+		
+		h.deal();
+		
+		h.printHand();
+		
+		h.newCard(2);
+		
+		System.out.println("-----");
+		
+		h.printHand();
+		
+	}
+
+}
