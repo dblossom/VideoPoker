a3bf2f8018262302422064c4ae5a1bf0830d39c3
dblossom <daniel.blossom@gmail.com>
2015-12-05 12:27:23 -0500

    a

---
+++ videopoker/videopoker/Suit.java
@@ -0,0 +1,12 @@
+package videopoker;
+
+/**
+ * A playing card only has 4 types of suits outlined below.
+ * 
+ * @author Dan Blossom
+ *
+ */
+enum Suit {
+	// the four suits.
+	HEART, DIAMOND, CLUB, SPADE
+}
